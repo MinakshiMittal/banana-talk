db573b4fd07849ae6899c785f547734bf4b57651
MinakshiMittal <minakshimittalm3@users.noreply.github.com>
2020-12-10 10:10:05 +0530

    done

--- minion.js
+++ minion.js
@@ -17,10 +17,10 @@ function clickHandler() {
     var inputText = inputTextarea.value;
 
     fetch(getTranslationURL(inputText))
-    .then(response => response.json)
-    .then(json => {
-        var translatedText = json.contents.translated;
-        outputDiv.innerText = translatedText;
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
     })
 
     .catch (errorHandler)
